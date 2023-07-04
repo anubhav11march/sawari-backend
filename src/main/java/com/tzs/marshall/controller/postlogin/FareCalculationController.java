da6543fbff494c6682aa13b28968905910c7145a
Priyam Srivastava <ipriyam26@gmail.com>
2023-07-04 16:16:00 +0530

    Fixed the pagination issue

--- src/main/java/com/tzs/marshall/controller/postlogin/FareCalculationController.java
+++ src/main/java/com/tzs/marshall/controller/postlogin/FareCalculationController.java
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-package com.tzs.marshall.controller.postlogin;
-
-import com.tzs.marshall.bean.Fare;
-import com.tzs.marshall.service.FareCalculationService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequestMapping({"/user", "/admin", "driver"})
-public class FareCalculationController {
-
-    @Autowired
-    private FareCalculationService fareCalculationService;
-
-    private static final Logger log = LoggerFactory.getLogger(FareCalculationController.class);
-
-
-    @RequestMapping(value = "/fare", method = RequestMethod.POST)
-    public Fare getEstimatedFare(@RequestBody Fare fare) {
-        Double distance = fare.getDistance();
-        Integer passenger = fare.getPassengers();
-        return fareCalculationService.getEstimatedFareByPassengerAndDistance(passenger, distance, null);
-
-    }
-}
-=======
 package com.tzs.marshall.controller.postlogin;
 
 import com.tzs.marshall.bean.Fare;
@@ -54,4 +25,3 @@ public class FareCalculationController {
 
     }
 }
->>>>>>> f881a63cb8a43ca17b12bd2c77df00db05cae227
--- src/main/java/com/tzs/marshall/service/FareCalculationService.java
+++ src/main/java/com/tzs/marshall/service/FareCalculationService.java
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-package com.tzs.marshall.service;
-
-import com.tzs.marshall.bean.DiscountConfig;
-import com.tzs.marshall.bean.Fare;
-
-import java.util.Map;
-
-public interface FareCalculationService {
-
-    Fare getEstimatedFareByPassengerAndDistance(Integer passenger, Double distance, Map<String, String> fareProperties);
-
-    DiscountConfig[] getDiscountConfig();
-
-    void updateDiscountConfig(DiscountConfig[] discountConfig);
-}
-=======
 package com.tzs.marshall.service;
 
 import com.tzs.marshall.bean.DiscountConfig;
@@ -32,4 +15,3 @@ public interface FareCalculationService {
 
     void updateDiscountConfig(DiscountConfig[] discountConfig);
 }
->>>>>>> f881a63cb8a43ca17b12bd2c77df00db05cae227
